f29dcc39937ef8d16076215b24aad3bc349a403a
Yuta Nakagawa <yuta@btrax.com>
2016-01-25 06:01:57 +0900

    play button animation

---
+++ Assets/Script/PlayToggleManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+[RequireComponent (typeof (Toggle))]
+public class PlayToggleManager : MonoBehaviour {
+	private Toggle toggle;
+	[SerializeField] private Image bgImage;
+
+	// Use this for initialization
+	void Start () {
+		toggle = GetComponent<Toggle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeToggleBGAlpha (bool isOn) {
+		Graphic bgGraphic = toggle.targetGraphic;
+
+		if (toggle.isOn) {
+			//Hide
+			bgGraphic.color = new Color(bgGraphic.color.r, bgGraphic.color.g, bgGraphic.color.b, 0.0f);
+		} else {
+			//Show
+			bgGraphic.color = new Color(bgGraphic.color.r, bgGraphic.color.g, bgGraphic.color.b, 255.0f);
+		}
+	}
+}
