8d208acfa6b2d248456d98def0d7359739b459b9
Yuta Nakagawa <yuta@btrax.com>
2016-01-29 01:08:17 +0900

    add animation

---
+++ Assets/Script/SplashLogoAnimationManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashLogoAnimationManager : MonoBehaviour {
+
+	[SerializeField] private Animator logoAnimator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnCompleteLogoAnimation () {
+		logoAnimator.SetBool("CompleteAllAnimations", true);
+	}
+}
